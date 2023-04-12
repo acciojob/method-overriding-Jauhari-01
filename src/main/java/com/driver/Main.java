043e3b0eb555619677067b7ee16d8ede3aa862e6
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2023-04-12 19:27:32 +0000

    removed comments

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,10 +13,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        // B obj = new B();
-        // System.out.println(B.meth());
         B b = new B();
-        // System.out.println(b.meth()); 
         b.meth();
 
     }
